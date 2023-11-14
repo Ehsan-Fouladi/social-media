a3a139d928834cf67a8e2a91fa480231ef84ec5e
Ehsan Fouladi <fooehan3@gmail.com>
2023-11-15 01:35:26 +0330

    desing and create login register

---
+++ pages/auth/Register.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="bg-gray-800 h-screen flex justify-center items-center">
+        <div class="bg-white rounded-md  w-full-screen shadow-2xl shadow-teal-600">
+            <div class="flex justify-center mt-10">
+                <span class="font-semibold text-3xl hover:text-yellow-300 hover:origin-top-left hover:rotate-12 delay-300 duration-200 ease-in">ثبت نام در سایت</span>
+            </div>
+            <form method="get" class="p-4 mt-36">
+                <input type="text" placeholder="ایمیل خود را وارد کنید." class="border-b-2 border-black outline-none w-full rounded px-10 py-2 text-purple-700 placeholder:text-gray-400">
+                <input type="password" placeholder="گذرواژه خود را وارد کنید." class="border-b-2 border-black outline-none w-full rounded px-10 py-2 text-purple-700 placeholder:text-gray-400 mt-10">
+                <input type="password" placeholder="گذرواژه دوم خود را وارد کنید." class="border-b-2 border-black outline-none w-full rounded px-10 py-2 text-purple-700 placeholder:text-gray-400 mt-10">
+                <div class="mt-6 flex flex-col md:flex-row md:justify-between">
+                    <div class="flex items-center mr-7">
+                        <input type="checkbox" class="w-4 h-4 rounded-full">
+                        <span class="mr-2 font-medium text-base"> مرا بخاطر بسپار</span>
+                    </div>
+                </div>
+                <div class="flex flex-col justify-center items-center mt-10">
+                    <NuxtLink class="bg-violet-700 py-3 px-10 rounded text-white outline-none hover:scale-110 delay-300 duration-200 ease-in">ثبت نام</NuxtLink>
+                    <NuxtLink to="/auth/login/" class="bg-gray-800 py-3 px-10 rounded text-white outline-none hover:scale-110 delay-300 duration-200 ease-in mt-3">ورود به سایت</NuxtLink>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
---
+++ pages/auth/Reset-password.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="bg-gray-800 h-screen flex justify-center items-center">
+        <div class="bg-white rounded-md w-full-screen shadow-2xl shadow-teal-600">
+            <div class="flex justify-center mt-10">
+                <span class="font-semibold text-3xl hover:text-yellow-300 hover:origin-top-left hover:rotate-12 delay-300 duration-200 ease-in">تغییر گذرواژه</span>
+            </div>
+            <form method="get" class="p-4 mt-36">
+                <input type="text" placeholder="ایمیل خود را وارد کنید." class="border-b-2 border-black outline-none w-full rounded px-10 py-2 text-purple-700 placeholder:text-gray-400 mt-10">
+
+                <div class="mt-20 flex justify-center items-center">
+                    <NuxtLink class="bg-violet-700 py-3 px-10 rounded text-white outline-none hover:scale-110 delay-300 duration-200 ease-in">تایید</NuxtLink>
+                </div>
+                <div class="flex flex-col md:flex-row items-center md:justify-evenly">
+                    <NuxtLink to="/auth/login/" class="bg-gray-800 py-3 px-10 rounded text-white outline-none hover:scale-110 delay-300 duration-200 ease-in mt-3 w-60 md:w-auto">ورود به سایت</NuxtLink>
+                    <NuxtLink to="/" class="bg-gray-800 py-3 px-10 rounded text-white outline-none hover:scale-110 delay-300 duration-200 ease-in mt-3 w-60 md:w-auto">بازگشت به سایت</NuxtLink>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
