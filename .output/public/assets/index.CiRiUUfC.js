8928a5eaf10d07f1f7a38958b41c2be521d2b844
Ehsan Fouladi <fooehan3@gmail.com>
2024-03-01 02:08:31 +0330

    update static file v.1

---
+++ .output/public/assets/index.CiRiUUfC.js
@@ -0,0 +1,1 @@
+import{g as a,i as t,j as o,c as s,t as r,u as n,o as c}from"./entry.BgHJnfNe.js";const i={__name:"index",setup(u){const e=a("");return t(()=>{e.value=o().params.category}),(l,p)=>(c(),s("div",null,r(n(e)),1))}};export{i as default};
---
+++ dist/assets/The-Northman.2ZJsgOxw.js
@@ -0,0 +1,1 @@
+import"./entry.BgHJnfNe.js";const t=""+new URL("The-Northman.BeMdvFsN.jpg",import.meta.url).href;export{t as _};
