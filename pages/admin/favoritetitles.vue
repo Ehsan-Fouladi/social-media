c0180b814ef43a86d086eaa5bec8ed88491d5976
Ehsan Fouladi <fooehan3@gmail.com>
2024-02-29 21:03:14 +0330

    complate admin and user panel

---
+++ pages/admin/favoritetitles.vue
@@ -0,0 +1,101 @@
+<template>
+    <div class="bg-gray-700 flex flex-col items-center absolute left-0 h-full w-full">
+       <div class="bg-gray-800 rounded-xl p-6 h-auto w-full md:w-4/6 mt-2 md:mt-5 md:mr-44 overflow-auto space-y-4">
+            <div class="bg-slate-900 p-5 rounded-lg w-full h-auto">
+                <NuxtLink to="/" class="text-white text-pretty text-xl font-medium capitalize tracking-wide md:tracking-widest">V/H/S/85 2023</NuxtLink>
+                <p class="mt-3 leading-7 tracking-tight text-white text-pretty md:text-balance select-none font-medium">پنج داستان از فیلم های ترسناک پیدا شده که از طریق یک مستند ساخته شده برای تلویزیون پرده برداری می شوند، ظاهر می شوند تا بینندگان را به سفری وحشتناک به درون شکم تیره دهه 1980 ببرند.</p>
+                <div class="mt-3 flex flex-row items-center gap-5">
+                    <span class="text-white text-pretty text-sm font-medium select-none">تاریخ: ۱۴۰۲/۰۷/۲۱</span>
+                    <span class="text-white text-pretty text-sm font-medium select-none">کاربر: سجاد</span>
+                    <span class="text-white text-pretty text-sm font-medium select-none">13 : لایک</span>
+                </div>
+            </div>
+            <div class="bg-slate-900 p-5 rounded-lg w-full h-auto">
+                <NuxtLink to="/" class="text-white text-pretty text-xl font-medium capitalize tracking-wide md:tracking-widest">V/H/S/85 2023</NuxtLink>
+                <p class="mt-3 leading-7 tracking-tight text-white text-pretty md:text-balance select-none font-medium">پنج داستان از فیلم های ترسناک پیدا شده که از طریق یک مستند ساخته شده برای تلویزیون پرده برداری می شوند، ظاهر می شوند تا بینندگان را به سفری وحشتناک به درون شکم تیره دهه 1980 ببرند.</p>
+                <div class="mt-3 flex flex-row items-center gap-5">
+                    <span class="text-white text-pretty text-sm font-medium select-none">تاریخ: ۱۴۰۲/۰۷/۲۱</span>
+                    <span class="text-white text-pretty text-sm font-medium select-none">کاربر: سجاد</span>
+                    <span class="text-white text-pretty text-sm font-medium select-none">13 : لایک</span>
+                </div>
+            </div>
+            <div class="bg-slate-900 p-5 rounded-lg w-full h-auto">
+                <NuxtLink to="/" class="text-white text-pretty text-xl font-medium capitalize tracking-wide md:tracking-widest">V/H/S/85 2023</NuxtLink>
+                <p class="mt-3 leading-7 tracking-tight text-white text-pretty md:text-balance select-none font-medium">پنج داستان از فیلم های ترسناک پیدا شده که از طریق یک مستند ساخته شده برای تلویزیون پرده برداری می شوند، ظاهر می شوند تا بینندگان را به سفری وحشتناک به درون شکم تیره دهه 1980 ببرند.</p>
+                <div class="mt-3 flex flex-row items-center gap-5">
+                    <span class="text-white text-pretty text-sm font-medium select-none">تاریخ: ۱۴۰۲/۰۷/۲۱</span>
+                    <span class="text-white text-pretty text-sm font-medium select-none">کاربر: سجاد</span>
+                    <span class="text-white text-pretty text-sm font-medium select-none">13 : لایک</span>
+                </div>
+            </div>
+            <div class="bg-slate-900 p-5 rounded-lg w-full h-auto">
+                <NuxtLink to="/" class="text-white text-pretty text-xl font-medium capitalize tracking-wide md:tracking-widest">V/H/S/85 2023</NuxtLink>
+                <p class="mt-3 leading-7 tracking-tight text-white text-pretty md:text-balance select-none font-medium">پنج داستان از فیلم های ترسناک پیدا شده که از طریق یک مستند ساخته شده برای تلویزیون پرده برداری می شوند، ظاهر می شوند تا بینندگان را به سفری وحشتناک به درون شکم تیره دهه 1980 ببرند.</p>
+                <div class="mt-3 flex flex-row items-center gap-5">
+                    <span class="text-white text-pretty text-sm font-medium select-none">تاریخ: ۱۴۰۲/۰۷/۲۱</span>
+                    <span class="text-white text-pretty text-sm font-medium select-none">کاربر: سجاد</span>
+                    <span class="text-white text-pretty text-sm font-medium select-none">13 : لایک</span>
+                </div>
+            </div>
+            <div class="bg-slate-900 p-5 rounded-lg w-full h-auto">
+                <NuxtLink to="/" class="text-white text-pretty text-xl font-medium capitalize tracking-wide md:tracking-widest">V/H/S/85 2023</NuxtLink>
+                <p class="mt-3 leading-7 tracking-tight text-white text-pretty md:text-balance select-none font-medium">پنج داستان از فیلم های ترسناک پیدا شده که از طریق یک مستند ساخته شده برای تلویزیون پرده برداری می شوند، ظاهر می شوند تا بینندگان را به سفری وحشتناک به درون شکم تیره دهه 1980 ببرند.</p>
+                <div class="mt-3 flex flex-row items-center gap-5">
+                    <span class="text-white text-pretty text-sm font-medium select-none">تاریخ: ۱۴۰۲/۰۷/۲۱</span>
+                    <span class="text-white text-pretty text-sm font-medium select-none">کاربر: سجاد</span>
+                    <span class="text-white text-pretty text-sm font-medium select-none">13 : لایک</span>
+                </div>
+            </div>
+            <div class="bg-slate-900 p-5 rounded-lg w-full h-auto">
+                <NuxtLink to="/" class="text-white text-pretty text-xl font-medium capitalize tracking-wide md:tracking-widest">V/H/S/85 2023</NuxtLink>
+                <p class="mt-3 leading-7 tracking-tight text-white text-pretty md:text-balance select-none font-medium">پنج داستان از فیلم های ترسناک پیدا شده که از طریق یک مستند ساخته شده برای تلویزیون پرده برداری می شوند، ظاهر می شوند تا بینندگان را به سفری وحشتناک به درون شکم تیره دهه 1980 ببرند.</p>
+                <div class="mt-3 flex flex-row items-center gap-5">
+                    <span class="text-white text-pretty text-sm font-medium select-none">تاریخ: ۱۴۰۲/۰۷/۲۱</span>
+                    <span class="text-white text-pretty text-sm font-medium select-none">کاربر: سجاد</span>
+                    <span class="text-white text-pretty text-sm font-medium select-none">13 : لایک</span>
+                </div>
+            </div>
+            <div class="bg-slate-900 p-5 rounded-lg w-full h-auto">
+                <NuxtLink to="/" class="text-white text-pretty text-xl font-medium capitalize tracking-wide md:tracking-widest">V/H/S/85 2023</NuxtLink>
+                <p class="mt-3 leading-7 tracking-tight text-white text-pretty md:text-balance select-none font-medium">پنج داستان از فیلم های ترسناک پیدا شده که از طریق یک مستند ساخته شده برای تلویزیون پرده برداری می شوند، ظاهر می شوند تا بینندگان را به سفری وحشتناک به درون شکم تیره دهه 1980 ببرند.</p>
+                <div class="mt-3 flex flex-row items-center gap-5">
+                    <span class="text-white text-pretty text-sm font-medium select-none">تاریخ: ۱۴۰۲/۰۷/۲۱</span>
+                    <span class="text-white text-pretty text-sm font-medium select-none">کاربر: سجاد</span>
+                    <span class="text-white text-pretty text-sm font-medium select-none">13 : لایک</span>
+                </div>
+            </div>
+            <div class="bg-slate-900 p-5 rounded-lg w-full h-auto">
+                <NuxtLink to="/" class="text-white text-pretty text-xl font-medium capitalize tracking-wide md:tracking-widest">V/H/S/85 2023</NuxtLink>
+                <p class="mt-3 leading-7 tracking-tight text-white text-pretty md:text-balance select-none font-medium">پنج داستان از فیلم های ترسناک پیدا شده که از طریق یک مستند ساخته شده برای تلویزیون پرده برداری می شوند، ظاهر می شوند تا بینندگان را به سفری وحشتناک به درون شکم تیره دهه 1980 ببرند.</p>
+                <div class="mt-3 flex flex-row items-center gap-5">
+                    <span class="text-white text-pretty text-sm font-medium select-none">تاریخ: ۱۴۰۲/۰۷/۲۱</span>
+                    <span class="text-white text-pretty text-sm font-medium select-none">کاربر: سجاد</span>
+                    <span class="text-white text-pretty text-sm font-medium select-none">13 : لایک</span>
+                </div>
+            </div>
+            <div class="bg-slate-900 p-5 rounded-lg w-full h-auto">
+                <NuxtLink to="/" class="text-white text-pretty text-xl font-medium capitalize tracking-wide md:tracking-widest">V/H/S/85 2023</NuxtLink>
+                <p class="mt-3 leading-7 tracking-tight text-white text-pretty md:text-balance select-none font-medium">پنج داستان از فیلم های ترسناک پیدا شده که از طریق یک مستند ساخته شده برای تلویزیون پرده برداری می شوند، ظاهر می شوند تا بینندگان را به سفری وحشتناک به درون شکم تیره دهه 1980 ببرند.</p>
+                <div class="mt-3 flex flex-row items-center gap-5">
+                    <span class="text-white text-pretty text-sm font-medium select-none">تاریخ: ۱۴۰۲/۰۷/۲۱</span>
+                    <span class="text-white text-pretty text-sm font-medium select-none">کاربر: سجاد</span>
+                    <span class="text-white text-pretty text-sm font-medium select-none">13 : لایک</span>
+                </div>
+            </div>
+            <div class="bg-slate-900 p-5 rounded-lg w-full h-auto">
+                <NuxtLink to="/" class="text-white text-pretty text-xl font-medium capitalize tracking-wide md:tracking-widest">V/H/S/85 2023</NuxtLink>
+                <p class="mt-3 leading-7 tracking-tight text-white text-pretty md:text-balance select-none font-medium">پنج داستان از فیلم های ترسناک پیدا شده که از طریق یک مستند ساخته شده برای تلویزیون پرده برداری می شوند، ظاهر می شوند تا بینندگان را به سفری وحشتناک به درون شکم تیره دهه 1980 ببرند.</p>
+                <div class="mt-3 flex flex-row items-center gap-5">
+                    <span class="text-white text-pretty text-sm font-medium select-none">تاریخ: ۱۴۰۲/۰۷/۲۱</span>
+                    <span class="text-white text-pretty text-sm font-medium select-none">کاربر: سجاد</span>
+                    <span class="text-white text-pretty text-sm font-medium select-none">13 : لایک</span>
+                </div>
+            </div>
+       </div>
+    </div>
+</template>
+<script setup>
+definePageMeta({
+    layout: 'admin',
+})
+</script>
---
+++ pages/admin/watchlist.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="bg-gray-700 flex flex-col items-center absolute left-0 h-full w-full">
+        <div class="bg-gray-800 mt-4 p-4 rounded-xl flex flex-wrap items-center gap-3 w-full md:w-3/4 md:mr-52">
+            <div class="bg-slate-900 px-5 py-1 rounded-lg cursor-pointer">
+                <span class="text-indigo-500 tracking-wide inset-3 font-medium text-sm capitalize">black bullet</span>
+            </div>
+            <div class="bg-slate-900 px-5 py-1 rounded-lg cursor-pointer">
+                <span class="text-indigo-500 tracking-wide inset-3 font-medium text-sm capitalize">black bullet</span>
+            </div>
+            <div class="bg-slate-900 px-5 py-1 rounded-lg cursor-pointer">
+                <span class="text-indigo-500 tracking-wide inset-3 font-medium text-sm capitalize">black bullet</span>
+            </div>
+            <div class="bg-slate-900 px-5 py-1 rounded-lg cursor-pointer">
+                <span class="text-indigo-500 tracking-wide inset-3 font-medium text-sm capitalize">black bullet</span>
+            </div>
+            <div class="bg-slate-900 px-5 py-1 rounded-lg cursor-pointer">
+                <span class="text-indigo-500 tracking-wide inset-3 font-medium text-sm capitalize">black bullet</span>
+            </div>
+            <div class="bg-slate-900 px-5 py-1 rounded-lg cursor-pointer">
+                <span class="text-indigo-500 tracking-wide inset-3 font-medium text-sm capitalize">black bullet</span>
+            </div>
+            <div class="bg-slate-900 px-5 py-1 rounded-lg cursor-pointer">
+                <span class="text-indigo-500 tracking-wide inset-3 font-medium text-sm capitalize">black bullet</span>
+            </div>
+            <div class="bg-slate-900 px-5 py-1 rounded-lg cursor-pointer">
+                <span class="text-indigo-500 tracking-wide inset-3 font-medium text-sm capitalize">black bullet</span>
+            </div>
+            <div class="bg-slate-900 px-5 py-1 rounded-lg cursor-pointer">
+                <span class="text-indigo-500 tracking-wide inset-3 font-medium text-sm capitalize">black bullet</span>
+            </div>
+            <div class="bg-slate-900 px-5 py-1 rounded-lg cursor-pointer">
+                <span class="text-indigo-500 tracking-wide inset-3 font-medium text-sm capitalize">black bullet</span>
+            </div>
+            <div class="bg-slate-900 px-5 py-1 rounded-lg cursor-pointer">
+                <span class="text-indigo-500 tracking-wide inset-3 font-medium text-sm capitalize">black bullet</span>
+            </div>
+            <div class="bg-slate-900 px-5 py-1 rounded-lg cursor-pointer">
+                <span class="text-indigo-500 tracking-wide inset-3 font-medium text-sm capitalize">black bullet</span>
+            </div>
+            <div class="bg-slate-900 px-5 py-1 rounded-lg cursor-pointer">
+                <span class="text-indigo-500 tracking-wide inset-3 font-medium text-sm capitalize">black bullet</span>
+            </div>
+            <div class="bg-slate-900 px-5 py-1 rounded-lg cursor-pointer">
+                <span class="text-indigo-500 tracking-wide inset-3 font-medium text-sm capitalize">black bullet</span>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+definePageMeta({
+    layout: 'admin',
+})
+</script>
