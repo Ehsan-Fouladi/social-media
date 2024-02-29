650b9ec52170de87eb6ef78554971dc8ae461fde
Mohammad Mahdi <matio.nora2000@gmail.com>
2024-02-29 23:05:21 +0330

    admin layout active page style fixed

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -56,38 +56,38 @@
                 </div>
             </div>
             <div class="flex flex-col justify-center items-center mt-8 w-full cursor-pointer space-y-4">
-                <NuxtLink to="/admin/" class="bg-gray-900 flex flex-row items-center gap-2 w-full p-5 text-indigo-600 mt-4">
+                <NuxtLink to="/admin/" class="flex flex-row items-center gap-2 w-full p-5 mt-4" :class="styled_active_page('admin')">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z" />
                     </svg>
                     <span>داشتبورد</span>
                 </NuxtLink>
-                <NuxtLink to="/admin/comments/" class="flex flex-row items-center gap-2 w-full p-5 text-yellow-300">
+                <NuxtLink to="/admin/comments/" class="flex flex-row items-center gap-2 w-full p-5" :class="styled_active_page('comments')">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 9.75a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 0 1 .778-.332 48.294 48.294 0 0 0 5.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
                     </svg>
                     <span>آخرین نظرات</span>
                 </NuxtLink>
-                <NuxtLink to="/admin/watchlist/" class="flex flex-row items-center gap-2 w-full p-5 text-yellow-300">
+                <NuxtLink to="/admin/watchlist/" class="flex flex-row items-center gap-2 w-full p-5" :class="styled_active_page('watchlist')">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
                     </svg>
                     <span>لیست تماشا</span>
                 </NuxtLink>
-                <NuxtLink to="/admin/favoritetitles/" class="flex flex-row items-center gap-2 w-full p-5 text-yellow-300">
+                <NuxtLink to="/admin/favoritetitles/" class="flex flex-row items-center gap-2 w-full p-5" :class="styled_active_page('favoritetitles')">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
                     </svg>
                     <span>عناوین مورد علاقه</span>
                 </NuxtLink>
-                <NuxtLink to="/admin/reset-password/" class="flex flex-row items-center gap-2 w-full p-5 text-yellow-300">
+                <NuxtLink to="/admin/reset-password/" class="flex flex-row items-center gap-2 w-full p-5" :class="styled_active_page('reset-password')">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z" />
                     </svg>
 
                     <span>تغییر گذرواژه</span>
                 </NuxtLink>
-                <NuxtLink to="/" class="flex flex-row items-center gap-2 w-full p-5 text-yellow-300">
+                <NuxtLink to="/" class="flex flex-row items-center bg-gra90  gap-2 w-full p-5 text-yellow-300">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
                     </svg>
@@ -101,5 +101,19 @@
 const menu = ref(true)
 function clickMenu() {
     menu.value = !menu.value
+}
+function currentPage() {
+    let paths = useRoute().fullPath.split('/')
+    
+    if (paths.length === 3) {
+        return 'admin'
+    }
+
+    let empty = paths.filter((path) => path.length > 0).length
+    return paths[paths.length - (empty)]
+}
+
+const styled_active_page = (page) => {
+    return page === currentPage() ? 'text-indigo-600 bg-gray-900' : 'text-yellow-300'
 }
 </script>
