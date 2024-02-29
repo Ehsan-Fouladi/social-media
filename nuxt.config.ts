9124ce5de77dd94c2676e076eb02b12ce66f9ce5
Ehsan Fouladi <fooehan3@gmail.com>
2024-03-01 00:57:24 +0330

    run dev

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,8 @@ export default defineNuxtConfig({
     },
   },
   app:{
+    baseURL: '/nuxt-github-pages/', // baseURL: '/<repository>/'
+    buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
     head: {
       link:[
         {rel:"stylesheet", href:"https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"}
