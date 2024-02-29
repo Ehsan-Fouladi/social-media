ea0fefcbc6a605a86cf22b52f06dd59ba94efb7a
Ehsan Fouladi <fooehan3@gmail.com>
2024-03-01 01:49:19 +0330

    update script

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,8 @@ export default defineNuxtConfig({
     },
   },
   app:{
+    baseURL: '/social-media/',
+    buildAssetsDir: 'assets',
     head: {
       link:[
         {rel:"stylesheet", href:"https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"}
