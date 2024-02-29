b5eb0b2da278733403915956d25b47633a1721ee
Ehsan Fouladi <fooehan3@gmail.com>
2024-03-01 01:54:04 +0330

    update script v1

--- .output/public/assets/subtle-prism.C9zJUCHW.js
+++
@@ -1,1 +0,0 @@
-import"./entry.mA9jW33F.js";const t=""+new URL("subtle-prism.CDoag3X0.svg",import.meta.url).href;export{t as _};
---
+++ dist/assets/subtle-prism.DcwBKS6I.js
@@ -0,0 +1,1 @@
+import"./entry.Ck78PxMR.js";const t=""+new URL("subtle-prism.CDoag3X0.svg",import.meta.url).href;export{t as _};
